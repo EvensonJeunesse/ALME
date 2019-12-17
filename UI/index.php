92b4c5dcc2ced7bc3d4b805f5015a585795c2d18
Evenson Jeunesse <evenson.jeunesse@etu.utc.fr>
2019-12-17 20:06:56 +0100

    website

---
+++ UI/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<!--
+	Cube by FreeHTML5.co
+	Twitter: http://twitter.com/gettemplateco
+	URL: http://freehtml5.co
+-->
+<html>
+	<head>
+	<?php include("head.html"); ?>
+	</head>
+	<body>
+
+	<div class="gtco-loader"></div>
+
+	<div id="page">
+
+<?php include("nav.html"); ?>
+
+		<header id="gtco-header" class="gtco-cover" role="banner">
+			<div class="gtco-container">
+				<div class="row">
+					<div class="col-md-12 col-md-offset-0 text-left">
+						<div class="display-t">
+							<div class="display-tc">
+								<div class="row">
+									<div class="col-md-5 text-center header-img animate-box">
+										<img src="images/cube.png" alt="Free HTML5 Website Template by FreeHTML5.co">
+									</div>
+									<div class="col-md-7 copy animate-box">
+										<h1>Active Localization of Mobile Equipment</h1>
+										<p>A proof of concept to show easy it might be to localize a wifi connected object.</p>
+										<p><a href="services.php" class="btn btn-white">Try the demo</a></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- END #gtco-header -->
+
+	
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
